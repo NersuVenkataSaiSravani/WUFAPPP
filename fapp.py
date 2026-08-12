29fea5b75dd9f9facc5092e8b9f91d3c9736f2e3
NersuVenkataSaiSravani <147939468+NersuVenkataSaiSravani@users.noreply.github.com>
2026-08-12 18:43:46 +0530

    Update fapp.py

--- fapp.py
+++ fapp.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home()
+def home():
     return "Hello! My web application is deployed successfully."
 
 if __name__ == "__main__":
