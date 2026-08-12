300206056c31c99641b2d7cadaa5251ff8b813c8
NersuVenkataSaiSravani <147939468+NersuVenkataSaiSravani@users.noreply.github.com>
2026-08-12 17:13:42 +0530

    Update fapp.py

--- fapp.py
+++ fapp.py
@@ -7,4 +7,4 @@ def home():
     return "Hello! My web application is deployed successfully."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)  #listen everyone on this server..
